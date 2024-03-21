deb757011bee9defa171c0c41ae830a5ce83d9ee
Victor Xavier <victorxavier530@gmail.com>
2024-03-21 14:56:01 -0300

    Initial commit

---
+++ src/main/java/br/dimap/Client.java
@@ -0,0 +1,72 @@
+package br.dimap;// A Java program for a Client
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    // initialize socket and input output streams
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+    private DataInputStream in = null;
+
+    // constructor to put ip address and port
+    public Client(String address, int port)
+    {
+        // establish a connection
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            // takes input from terminal
+            input = new DataInputStream(System.in);
+
+            // sends output to the socket
+            out = new DataOutputStream(
+                    socket.getOutputStream());
+
+            // takes input from server
+            in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+        }
+        catch (UnknownHostException u) {
+            System.out.println(u);
+            return;
+        }
+        catch (IOException i) {
+            System.out.println(i);
+            return;
+        }
+
+        // string to read message from input
+        String line = "";
+
+        // keep reading until "Over" is input
+        while (!line.equals("wp")) {
+            System.out.print("Human: ");
+            try {
+                line = input.readLine();
+                out.writeUTF(line);
+                System.out.println(in.readUTF());
+            }
+            catch (IOException i) {
+                System.out.println(i);
+            }
+
+        }
+
+        // close the connection
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        }
+        catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        Client client = new Client("127.0.0.1", 8080);
+    }
+}
